3da87a9faa137857b8a7c99713b6cb9cd52f18cf
bhavana594 <bhavanadyavannanavar@gmail.com>
2025-11-03 20:41:00 +0530

    Create Multiplication.c

---
+++ Multiplication.c
@@ -0,0 +1,14 @@
+//Take two numbers num1 and num2 as input and multiply them
+#include <stdio.h>
+
+int main()
+{
+    int num1, num2;
+    scanf("%d", &num1);
+    scanf("%d", &num2);
+
+    int product = num1 * num2;
+    printf("%d", product);
+
+    return 0;
+}
