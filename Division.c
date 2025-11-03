fab4131399e264a236f0bab0e60d6e1966942487
bhavana594 <bhavanadyavannanavar@gmail.com>
2025-11-03 20:43:17 +0530

    Create Division.c

---
+++ Division.c
@@ -0,0 +1,14 @@
+//Division of two number taken By user Input
+#include <stdio.h>
+
+int main()
+{
+    int num1, num2;
+    scanf("%d", &num1);
+    scanf("%d", &num2);
+
+    int div = num1 / num2;
+    printf("%d", div);
+
+    return 0;
+}
