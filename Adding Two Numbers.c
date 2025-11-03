d9bc576e5b1c24b60d553438ad47942a31ca251f
bhavana594 <bhavanadyavannanavar@gmail.com>
2025-11-03 20:59:17 +0530

    Create Adding Two Numbers.c

---
+++ Adding Two Numbers.c
@@ -0,0 +1,14 @@
+//Take two numbers num1 and num2 as input from stdin and print the sum of those two numbers.
+#include <stdio.h>
+
+int main()
+{
+    int num1, num2;
+    scanf("%d", &num1);
+    scanf("%d", &num2);
+
+    int Sum = num1 + num2;
+    printf("%d", Sum);
+
+    return 0;
+}
