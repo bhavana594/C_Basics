ade5be3244debfa4400fba41585768e4760fb2aa
bhavana594 <bhavanadyavannanavar@gmail.com>
2025-11-03 15:23:28 +0530

    Create Second _number.c

---
+++ Second _number.c
@@ -0,0 +1,11 @@
+//Average of two numbers is X. The first is X.FInd the Second Number y.
+
+#include<stdio.h>
+int main()
+{
+int x,y,z;
+scanf("%d%d",&x&y);
+z = (2*x)-y;
+printf("%d",z);
+return 0;
+}
