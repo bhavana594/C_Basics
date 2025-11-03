1b8b8e52e4ad0234afbfe34bc1462f6d3b481dd2
bhavana594 <bhavanadyavannanavar@gmail.com>
2025-11-03 22:43:48 +0530

    Create distance_between_twopoints.c

---
+++ distance_between_twopoints.c
@@ -0,0 +1,13 @@
+//Finding the distance between two points
+#include <stdio.h>
+#include <math.h>
+int main()
+{
+    int x1, y1, x2, y2;
+    scanf("%d %d %d %d", &x1, &y1, &x2, &y2);
+
+    float result = sqrt(((x2 - x1) * (x2 - x1)) + ((y2 - y1) * (y2 - y1)));
+    printf("%.4f", result);
+
+    return 0;
+}
