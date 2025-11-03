327d044973eb5f4069a428a446d89f0b402da456
bhavana594 <bhavanadyavannanavar@gmail.com>
2025-11-03 20:24:52 +0530

    Create Arithemetic_operation.c

---
+++ Arithemetic_operation.c
@@ -0,0 +1,23 @@
+//Perform the arithmetic  operation on given two number num1,num2.
+
+#include <stdio.h>
+
+int main()
+{
+    int num1, num2;
+    scanf("%d %d", &num1, &num2);
+
+    int sum = num1 + num2;
+    int difference = num1 - num2;
+    int product = num1 * num2;
+    int quotient = num1 / num2;
+    int remainder = num1 % num2;
+
+    printf("Sum: %d\n", sum);
+    printf("Difference: %d\n", difference);
+    printf("Product: %d\n", product);
+    printf("Quotient: %d\n", quotient);
+    printf("Remainder: %d\n", remainder);
+
+    return 0;
+}
