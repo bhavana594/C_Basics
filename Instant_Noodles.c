5d685a44e4927cd2c6961dcd5b3b44e6e728ea8c
bhavana594 <bhavanadyavannanavar@gmail.com>
2025-11-03 15:53:01 +0530

    Update Instant_Noodles.c

--- Instant_Noodles.c
+++ Instant_Noodles.c
@@ -1,6 +1,6 @@
-Ravi has invented 1-minute Instant Noodles. As the name suggests, each packet takes exactly 1 minute to cook. 
-Ravi's Restaurant has X stoves and only 1 packet can be cooked on a single stove at any minute.
-How many customers can Ravi serve in Y minutes if each customer orders exactly 1 packet of noodles?
+//Ravi has invented 1-minute Instant Noodles. As the name suggests, each packet takes exactly 1 minute to cook. 
+//Ravi's Restaurant has X stoves and only 1 packet can be cooked on a single stove at any minute.
+//How many customers can Ravi serve in Y minutes if each customer orders exactly 1 packet of noodles?
 
 #include<stdio.h>
 int main()
