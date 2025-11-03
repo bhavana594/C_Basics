af14e7e5a6951916718bc00338b678e89d42df06
bhavana594 <bhavanadyavannanavar@gmail.com>
2025-11-03 15:33:33 +0530

    Create Find_Hypotenuse.c

---
+++ Find_Hypotenuse.c
@@ -0,0 +1,10 @@
+//Find Hypotenuse of the right angled Triangle with two given sides X,Y.
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+int x,y;
+scanf("%d %d",&x,&y);
+printf("%d.2f",sqrt(x*x+y*y));
+return 0;
+}
