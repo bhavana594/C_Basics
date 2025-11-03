1fd9c9a4c060a756e6e9156534d57c2754499f14
bhavana594 <bhavanadyavannanavar@gmail.com>
2025-11-03 15:45:25 +0530

    Create Heron's_Formula.c

---
+++ Heron's_Formula.c
@@ -0,0 +1,15 @@
+Find the area of Triangle using Heron's formula.
+Hint:Heron's Formula= sqrt(s*(s-a)*(s-b)*(s-c).where a,b,c are sides.
+
+  #include<stdio.h>
+
+#include<math.h>
+int main()
+{
+int a,b,c;
+scanf("%d%d%d",&a,&b,&c);
+doubles (a+b+c)/2.0;
+double area = sqrt(s*(s-a)*(s-b)*(s-c));
+printf("%.4f", area);
+return 0;
+}
